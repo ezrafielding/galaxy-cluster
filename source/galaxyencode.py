bae84865cdb585798d3c570034e336e477d19557
hussein <eslam@idia.ac.za>
2022-10-20 17:26:09 +0200

    ez changes

--- source/galaxyencode.py
+++ source/galaxyencode.py
@@ -9,21 +9,17 @@ class GalaxyEncoder(Model):
             layers.InputLayer(input_shape=(56,56,1)),
             layers.Conv2D(16, (3,3), 1, padding="same", activation="relu"),
             layers.MaxPool2D((2,2), padding="same", strides=2),
-            # layers.Conv2D(8, (3,3), 1, padding="same", activation="relu"),
-            # layers.MaxPool2D((2,2), padding="same", strides=2),
-            # layers.Conv2D(8, (3,3), 1, padding="same", activation="relu"),
-            # layers.MaxPool2D((2,2), padding="same", strides=2),
+            layers.Conv2D(8, (3,3), 1, padding="same", activation="relu"),
+            layers.MaxPool2D((2,2), padding="same", strides=2),
             layers.Flatten()
         ])
         self.decoder = tf.keras.Sequential ([
             layers.InputLayer(input_shape=(1568)),
-            layers.Reshape((28,28,2)),
+            layers.Reshape((14, 14, 8)),
             layers.UpSampling2D((2,2)),
             layers.Conv2DTranspose(8, (3,3), 1, padding="same", activation="relu"),
-            # layers.UpSampling2D((2,2)),
-            # layers.Conv2DTranspose(8, (3,3), 1, padding="same", activation="relu"),
-            # layers.UpSampling2D((2,2)),
-            # layers.Conv2DTranspose(16, (3,3), 1, padding="same", activation="relu"),
+            layers.UpSampling2D((2,2)),
+            layers.Conv2DTranspose(16, (3,3), 1, padding="same", activation="relu"),
             layers.Conv2D(1, (3,3), 1, padding="same", activation="sigmoid")
         ])
 
